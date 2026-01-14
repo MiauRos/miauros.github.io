7220b81d644e923f11c96f9ae0f48b8968fdbe82
Mauricio Rosales <maurirosrdz@gmail.com>
2026-01-14 14:57:41 -0600

    feat: add web themes colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,6 +14,36 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'system',
-  },
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          background: '#d6def0',
+          text: '#14161a',
+          primary: '#1453db',   // Verde principal
+          secondary: '#a9b7cb', // Verde suave
+          accent: '#3bde77',    // Verde acento
+          error: '#E53935',
+          info: '#1E88E5',
+          success: '#43A047',
+          warning: '#FB8C00'
+        }
+      },
+      dark: {
+        dark: true,
+        colors: {
+          background: '#0F172A',
+          text: '#E5E7EB',
+          primary: '#2563EB',
+          secondary: '#334155',
+          accent: '#22C55E',
+          error: '#EF5350',
+          info: '#42A5F5',
+          success: '#66BB6A',
+          warning: '#FFA726'
+        }
+      }
+    }
+  }
 })
