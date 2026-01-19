33cc8f2e00ae8103b4c1bbc7b4f2278efd250512
Mauricio Rosales <maurirosrdz@gmail.com>
2026-01-19 14:18:07 -0600

    feat: add something to projects page #pr #dev

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,13 +1,34 @@
-<script setup>
-
-</script>
-
 <template>
   <v-container>
-    Projects
+    <v-card v-for="item in projects">
+      <v-card-title>
+        {{ item.name}}
+      </v-card-title>
+      <v-card-text>
+        {{ item.description }}
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
-<style scoped>
+<script setup>
+  import {ref} from "vue";
 
+  const projects = ref([
+    {
+      name: 'Project 1',
+      description: 'Project 1',
+    },
+    {
+      name: 'Project 2',
+      description: 'Project 2',
+    },
+    {
+      name: 'Project 3',
+      description: 'Project 3',
+    }
+  ])
+</script>
+
+<style scoped>
 </style>
